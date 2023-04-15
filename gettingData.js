04b0ff07c84ba3fb39ad3d50ced775cea4f3c59f
Maria <zeldacamila0913@gmail.com>
2023-04-15 14:26:38 -0500

    uploaded script to get latitude and longitude with an Address

---
+++ gettingData.js
@@ -0,0 +1,20 @@
+// Define the address you want to geocode
+var address = "1600 Amphitheatre Parkway, Mountain View, CA";
+
+// Create a geocoder object
+var geocoder = new google.maps.Geocoder();
+
+// Call the geocode() method with the address and a callback function
+geocoder.geocode({address: address}, function(results, status) {
+  if (status === google.maps.GeocoderStatus.OK) {
+    // Extract the latitude and longitude from the geocoding result
+    var lat = results[0].geometry.location.lat();
+    var lng = results[0].geometry.location.lng();
+
+    // Do something with the latitude and longitude
+    console.log("Latitude: " + lat);
+    console.log("Longitude: " + lng);
+  } else {
+    console.error("Geocoding failed due to: " + status);
+  }
+});
